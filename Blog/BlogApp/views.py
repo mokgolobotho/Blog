6f79af2b564635db5d458b5fdae328e3fca3d053
Thabang <mokgolobothothabang@gmail.com>
2024-12-05 16:00:19 +0200

    User can login

--- Blog/BlogApp/views.py
+++ Blog/BlogApp/views.py
@@ -1,14 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def authView(request):
+    print("debug1")
     if request.method == "POST":
+        print("debug2 ")
         form = UserCreationForm(request.POST or None)
         if form.is_valid():
             form.save()
-            return render(request, "BlogApp:login")
+            return redirect("BlogApp:login")
     else:
         form = UserCreationForm()
     return render(request, "registration/signup.html", {"form": form})
