d5403a7e271a3f7ca9e614c663283aeb2c3bd918
Thabang <mokgolobothothabang@gmail.com>
2024-12-05 12:42:59 +0200

    Super user setup

--- Blog/BlogApp/urls.py
+++ Blog/BlogApp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
-
+from .views import authView,home
 urlpatterns = [
+    path("", home, name="home"),
+    path("signup/", authView, name="authView"),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
--- Blog/BlogApp/views.py
+++ Blog/BlogApp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def authView(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            return render(request, "BlogApp:login")
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/signup.html", {"form": form})
+
+@login_required
+def home(request):
+    return render(request, "home.html", {})
